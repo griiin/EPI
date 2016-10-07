e64e475bf8034c32237a6cfa10ec908c6ca47397
griiin <julien.nigri@gmail.com>
2016-10-07 22:30:17 +0100

    15.1

--- 15.1/main.rs
+++ 15.1/main.rs
@@ -1,24 +1,29 @@
-fn is_bst(v: &Vec<i32>, i: usize) -> bool {
+fn fill_in_order(v: &Vec<i32>, i: usize, r: &mut Vec<i32>) {
     if i > v.len() - 1 || v[i] == -1 {
-        return true;
+        return;
     }
     let left_i = (i * 2) + 1;
     let right_i = (i * 2) + 2;
-    if left_i < v.len() && v[left_i] != -1 {
-        if v[left_i] > v[i] {
+    fill_in_order(v, left_i, r);
+    r.push(v[i]);
+    fill_in_order(v, right_i, r);
+}
+
+fn is_in_order(v: Vec<i32>) -> bool {
+    let mut last = -1;
+    for c in v {
+        if c < last {
             return false;
         }
+        last = c;
     }
-    if right_i < v.len() && v[right_i] != -1 {
-        if v[right_i] < v[i] {
-            return false;
-        }
-    }
-    is_bst(v, (i * 2) + 1) && is_bst(v, (i * 2) + 2)
+    true
 }
 
 fn main() {
     let bst = vec![19,7,43,3,11,23,47,2,5,-1,17,-1,37,-1,53];
-    let r = is_bst(&bst, 0);
-    println!("{}", r);
+    let mut in_order:Vec<i32> = Vec::new();
+    fill_in_order(&bst, 0, &mut in_order);
+    let r = is_in_order(in_order);
+    println!("{:?}", r);
 }
