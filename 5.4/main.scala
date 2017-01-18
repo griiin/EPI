0035db3ac3405f23095b439d92ccabe7eb685e15
griiin <julien.nigri@gmail.com>
2017-01-18 21:27:46 +0000

    5.4

--- 5.4/main.scala
+++ 5.4/main.scala
@@ -4,14 +4,31 @@ object Main {
     case n => getBitWeigth(nb & (nb - 1), weight + 1)
   }
 
+  def getClosestIntByWeight(nb: Int, d: Int = 1): Int = (nb, d) match {
+    case (0, _) => 1
+    case _ => {
+      val left = nb - d;
+      val right = nb + d;
+      if (left > 0  && getBitWeigth(nb) == getBitWeigth(left)) {
+        left
+      } else if (getBitWeigth(nb) == getBitWeigth(right)) {
+        right
+      } else {
+        getClosestIntByWeight(nb, d + 1)
+      }
+    }
+  }
+
   def getBitWeigth(nb: String): Int = {
     getBitWeigth(Integer.parseInt(nb, 2))
   }
 
   def main(args: Array[String]): Unit = {
-    println(getBitWeigth("1001"));
+    /*println(getBitWeigth("1001"));
     println(getBitWeigth("1001001"));
     println(getBitWeigth("1111"));
-    println(getBitWeigth("0"));
+    println(getBitWeigth("0"));*/
+    println(getClosestIntByWeight(6))
+    println(getClosestIntByWeight(7))
   }
 }
